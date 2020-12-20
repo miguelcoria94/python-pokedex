713e694f57b032f0d556264b4fb81c4d96681626
miguelcoria94 <miguelcoria94@gmail.com>
2020-12-20 16:41:10 -0600

    convert gray pickachu

--- JPGtoPNGconverter.py
+++ JPGtoPNGconverter.py
@@ -1,5 +1,5 @@
 from PIL import Image, ImageFilter
 
 img = Image.open('./Pokedex/pikachu.jpg')
-filtered_img = img.filter(ImageFilter.BLUR)
-filtered_img.save("Blur.png", 'png')
+filtered_img = img.convert('L')
+filtered_img.save("grey.png", 'png')
